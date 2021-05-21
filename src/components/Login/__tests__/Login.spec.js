d6548066a26c776dd1c845761548c2cbb1860fc0
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2021-05-20 19:59:19 -0500

    Update

--- src/components/Login/__tests__/Login.spec.js
+++ src/components/Login/__tests__/Login.spec.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Login from '../index';
 
 describe('Login', () => {
@@ -15,5 +15,24 @@ describe('Login', () => {
 		render(<Login />);
 		expect(screen.getByTitle(/username/i)).toBeInTheDocument();
 		expect(screen.getByTitle(/password/i)).toBeInTheDocument();
+		expect(screen.getByPlaceholderText(/username/i)).toBeInTheDocument();
+		expect(screen.getByPlaceholderText(/password/i)).toBeInTheDocument();
+	});
+
+	it('Should type name and password fields', () => {
+		render(<Login />);
+
+    const fakeData = {
+      name: 'Bernardo Aguayo',
+      password: 'ohiifasfoafsnoasdk'
+    }
+		const nameField = screen.getByPlaceholderText(/username/i);
+    const passwordField = screen.getByPlaceholderText(/password/i)
+
+    fireEvent.change(nameField, { target: { value: fakeData.name } })
+    fireEvent.change(passwordField, { target: { value: fakeData.password } })
+    
+    expect(screen.getByDisplayValue(fakeData.name)).toBeInTheDocument()
+    expect(screen.getByDisplayValue(fakeData.password)).toBeInTheDocument()
 	});
 });
